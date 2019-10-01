8e77ff77870553a247db5c00cdc593c9cab32f98
binary-manu <47382117+binary-manu@users.noreply.github.com>
2019-10-01 21:13:34 +0200

    [vagrant] Workaround Vagrant 2.2.5 Ansible install
    Rather than depending on the `ansible_local` provisioner to
    install Ansible, we install it on our own. This is because Vagrant
    2.2.5 fails to install it.

--- vagrant/prepare-box-for-arch-ansible.sh
+++ vagrant/prepare-box-for-arch-ansible.sh
@@ -5,16 +5,12 @@ if [ -n "$http_proxy" ]; then
     sed --in-place -e 's|^#\(XferCommand = /usr/bin/curl.*\)$|\1|' /etc/pacman.conf
 fi
 
-# Perform a full system update. This is required by both arch-ansible (because
-# the configuration steps assume that pacstrap synced the indices) and by the
-# ansible_local provisioner (which updates the indices and installs Ansible,
-# but does not perform a full update so we may end up with a broken apartially
-# updated system).
-# It also installs the following packages because, again, the bootstrap phase
-# of arch-ansible would have done that:
-#   - base-devel
-#   - networkmanager
-pacman -Syu --noconfirm --needed base-devel networkmanager
+# Perform a full system update. This is required by arch-ansible, because the
+# configuration steps assume that pacstrap synced the indices). It also
+# installs some packages because, again, the bootstrap phase of arch-ansible
+# would have done that. Ansible is installed here because Vagrant 2.2.5 fails
+# to install it.
+pacman -Syu --noconfirm --needed base-devel networkmanager ansible
 
 # Enable and start NetworkManager, while removing systemd-netword profiles.
 systemctl enable NetworkManager.service
