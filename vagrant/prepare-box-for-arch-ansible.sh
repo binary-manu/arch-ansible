d90e7e4d561bf26869ec7db9fcd77e94e809bcad
binary-manu <47382117+binary-manu@users.noreply.github.com>
2019-10-07 11:01:01 +0200

    [vagrant] Add packages left out from base

--- vagrant/prepare-box-for-arch-ansible.sh
+++ vagrant/prepare-box-for-arch-ansible.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+# These packages are no longer pulled in by the "base" package. So we install
+# them explicitly. `base` is present because the initial Vagrant image may
+# still be based on the group rather than on the package.
+EXTRA_BASE_PACKAGES="
+    base
+    cryptsetup
+    device-mapper
+    dhcpcd
+    e2fsprogs
+    inetutils
+    jfsutils
+    linux
+    linux-firmware
+    logrotate
+    lvm2
+    man-db
+    man-pages
+    mdadm
+    nano
+    netctl
+    perl
+    sysfsutils
+    texinfo
+    usbutils
+    vi
+    xfsprogs
+"
+
 # If the proxy has been provisioned, change pacman's transfer command to curl
 if [ -n "$http_proxy" ]; then
     sed --in-place -e 's|^#\(XferCommand = /usr/bin/curl.*\)$|\1|' /etc/pacman.conf
@@ -10,7 +38,8 @@ fi
 # installs some packages because, again, the bootstrap phase of arch-ansible
 # would have done that. Ansible is installed here because Vagrant 2.2.5 fails
 # to install it.
-pacman -Syu --noconfirm --needed base-devel networkmanager ansible
+pacman -Syu --noconfirm --needed base-devel networkmanager ansible \
+    $EXTRA_BASE_PACKAGES
 
 # Enable and start NetworkManager, while removing systemd-netword profiles.
 systemctl enable NetworkManager.service
