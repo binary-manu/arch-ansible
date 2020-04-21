bc15a406feecbe78a0f1b4add87a576c7a1beef4
binary-manu <47382117+binary-manu@users.noreply.github.com>
2020-04-21 21:44:55 +0200

    [vagrant] Wait for reflector to write a new mirrorlist
    Sometimes reflector fails to update the mirrors, and the currently
    synced DB's can be out of sync with the first mirror in the list.
    As a workaround, the first sysupgrade forces a repo sync (-Syy). When
    reflector does its job, it is harmless as new repos are likely to be
    downloaded anyway.

--- vagrant/prepare-box-for-arch-ansible.sh
+++ vagrant/prepare-box-for-arch-ansible.sh
@@ -35,12 +35,17 @@ if [ -n "$http_proxy" ]; then
     sed --in-place -e 's|^#\(XferCommand = /usr/bin/curl.*\)$|\1|' /etc/pacman.conf
 fi
 
+# The base Vagrant image uses reflector to generate a new mirrorlist
+# on the first boot. This means we must wait for it to finish before
+# upgrading the system.
+systemctl start reflector-init
+
 # Perform a full system update. This is required by arch-ansible, because the
 # configuration steps assume that pacstrap synced the indices. It also
 # installs some packages because, again, the bootstrap phase of arch-ansible
 # would have done that. Ansible is installed here because Vagrant 2.2.5 fails
 # to install it.
-pacman -Syu --noconfirm --needed base-devel networkmanager ansible \
+pacman -Syyu --noconfirm --needed base-devel networkmanager ansible \
     $EXTRA_BASE_PACKAGES
 
 # Enable and start NetworkManager, while removing systemd-netword profiles.
