afea7b872b9dcad2c6e66dea4ee8f84f2661002b
binary-manu <47382117+binary-manu@users.noreply.github.com>
2021-02-28 21:48:09 +0100

    [packer] Explicitly delete SSH authorized keys during cleanup

--- packer/clean.sh
+++ packer/clean.sh
@@ -4,3 +4,11 @@ set -e
 
 pacman -Rs --noconfirm ansible
 rm -rf /root/.ansible
+
+# Packer's QEMU builder seems to fail to remove the SSH key
+# even when ssh_clear_authorized_keys is true. And virtualbox-iso
+# always tries to remove it even when ssh_clear_authorized_keys is
+# false and if the file does not exist it prints errors.
+# To make everyone happy, we remove the keys here but keep the empty
+# file around.
+truncate -s 0 /root/.ssh/authorized_keys
