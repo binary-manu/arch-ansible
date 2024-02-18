b484120685e6bf17df54d0f5f583565570ec0d8d
binary-manu <47382117+binary-manu@users.noreply.github.com>
2024-02-18 12:35:22 +0100

    [ci] Add a container with CircleCI machine runner installed

---
+++ ci/entry.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+if [ "$(ps --no-headers -p $$ -o euid -n)" -eq 0 ]; then
+    if [ -z "$PUID" -o -z "$PGID" ]; then
+        echo "PUID and PGID need to be defined" >&2
+        exit 1
+    fi
+    rm -f /run/dbus/pid && dbus-daemon --system
+    libvirtd --daemon
+    virsh net-create /etc/libvirt/qemu/networks/default.xml
+    exec setpriv --keep-groups --reuid "$PUID" --regid "$PGID" "$0" "$@"
+fi
+
+cd
+start-stop-daemon -b -m -p /tmp/pkgproxy.pid -O "$HOME/pkgproxy.log" -x "$PKGPROXY" -S -- -keep-cache
+exec "$@"
