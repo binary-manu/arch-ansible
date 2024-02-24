c2d6a1dcb6efa3e32cb3bbb856bf79a38ac0f260
binary-manu <47382117+binary-manu@users.noreply.github.com>
2024-02-24 12:50:33 +0100

    [ci] Add start script with appropriate podman parameters

---
+++ ci/start.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+HERE="$(realpath "$(dirname "$0")")"
+BASEDIR="$(realpath "$HERE/..")"
+HOMEDIR="$(realpath "$HERE/storage")"
+IMAGE="ghcr.io/binary-manu/arch-ansible-ci:circleci"
+ADD_CAPS="NET_ADMIN,NET_RAW"
+HOSTNAME="linux-runner"
+KEEP_SUP_GROUPS="--annotation=run.oci.keep_original_groups=1"
+
+if [ -z "$FOREGROUND" ]; then
+    FOREGROUND=-d
+else
+    FOREGROUND="--rm -it"
+fi
+
+podman run $FOREGROUND \
+    --replace \
+    --name ci \
+    --hostname "$HOSTNAME" \
+    -e PUID="$(id -u)" \
+    -e PGID="$(id -g)" \
+    --device=/dev/kvm:rw \
+    --device=/dev/vboxdrv:rw \
+    --device=/dev/vboxdrvu:rw \
+    --device=/dev/vboxnetctl:rw \
+    --device=/dev/net/tun:rw \
+    --userns keep-id \
+    --user root \
+    --cap-add="$ADD_CAPS" \
+    --security-opt=unmask=/proc/sys \
+    -v "$BASEDIR:/src" \
+    -v "$HOMEDIR:/home" \
+    -v "$HERE/config.yaml:/etc/circleci-runner/circleci-runner-config.yaml" \
+    $KEEP_SUP_GROUPS \
+    "$IMAGE" "$@"
