cad6e35353ffb739c1455b84508e0eb3aed266b4
binary-manu <47382117+binary-manu@users.noreply.github.com>
2025-02-02 19:51:41 +0100

    Grab HashiCorp packages from the official repo
    In order to access the latest versions.

--- ci/entry.sh
+++ ci/entry.sh
@@ -3,17 +3,14 @@
 set -ex
 
 if [ "$(ps --no-headers -p $$ -o euid -n)" -eq 0 ]; then
-    if [ -z "$PUID" -o -z "$PGID" ]; then
-        echo "PUID and PGID need to be defined" >&2
-        exit 1
-    fi
     rm -f /run/dbus/pid && dbus-daemon --system
     libvirtd --daemon
     virsh net-create /etc/libvirt/qemu/networks/default.xml
-    exec setpriv --keep-groups --reuid "$PUID" --regid "$PGID" "$0" "$@"
+    exec setpriv --keep-groups --reuid "$(id -u ci)" --regid "$(id -g ci)" "$0" "$@"
 fi
 
+HOME=/home/$CI_USER
 cd
 start-stop-daemon -b -m -p /tmp/pkgproxy.pid -O "$HOME/pkgproxy.log" -x "$PKGPROXY" -S -- -keep-cache
-cp -v /config/.[a-z]* /opt/gh/
+cp -v /config/.[a-z]* "$HOME/gh/"
 exec "$@"
--- ci/start.sh
+++ ci/start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 HERE="$(realpath "$(dirname "$0")")"
-HOMEDIR="$(realpath "$HERE/storage")"
 IMAGE="ghcr.io/binary-manu/arch-ansible-ci:github"
 ADD_CAPS="NET_ADMIN,NET_RAW"
 HOSTNAME="linux-runner"
 KEEP_SUP_GROUPS="--annotation=run.oci.keep_original_groups=1"
+CI_UID=1000
+CI_GID=1000
 
 if [ -z "$FOREGROUND" ]; then
     FOREGROUND=-d
@@ -17,18 +18,15 @@ podman run $FOREGROUND \
     --replace \
     --name ci \
     --hostname "$HOSTNAME" \
-    -e PUID="$(id -u)" \
-    -e PGID="$(id -g)" \
     --device=/dev/kvm:rw \
     --device=/dev/vboxdrv:rw \
     --device=/dev/vboxdrvu:rw \
     --device=/dev/vboxnetctl:rw \
     --device=/dev/net/tun:rw \
-    --userns keep-id \
+    --userns keep-id:uid=$CI_GID,gid=$CI_GID \
     --user root \
     --cap-add="$ADD_CAPS" \
     --security-opt=unmask=/proc/sys \
     -v "$HERE/config:/config" \
-    -v "$HOMEDIR:/home" \
     $KEEP_SUP_GROUPS \
     "$IMAGE" "$@"
